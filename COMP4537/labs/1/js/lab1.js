6d7118be384cf263aa43a0fe1a5d88188d5573d5
Steven Chow <steven.chow@live.ca>
2023-09-15 10:49:08 -0700

    Add comments.

--- COMP4537/labs/1/js/lab1.js
+++ COMP4537/labs/1/js/lab1.js
@@ -1,10 +1,19 @@
+/**
+ * A class that represents a Note object.
+ */
 class Note {
+    // Constructor for creating a Note object
+    // Parameters:
+    //   key: Identifier for the group of notes
+    //   id: Unique identifier for the individual note (default: current timestamp)
+    //   value: The content of the note (default: empty string)
     constructor(key, id = new Date().getTime().toString(), value = "") {
         this.id = id;
         this.value = value;
         this.key = key;
     }
 
+    // Method to remove a note from both the DOM and local storage
     remove() {
         // Remove note from DOM
         const noteContainer = document.getElementById(`${this.key}-${this.id}`);
@@ -18,8 +27,9 @@ class Note {
         localStorage.setItem(this.key, JSON.stringify(updatedNotesArr));
     }
 
+    // Method to create a note element for editing
     createWriterNoteElement() {
-        // Create outter div, textarea and remove button
+        // Create outer div, textarea and remove button
         const noteContainer = document.createElement("div");
         const textArea = document.createElement("textarea");
         const removeButton = document.createElement("button");
@@ -38,11 +48,11 @@ class Note {
         textArea.value = this.value;
         textArea.placeholder = `Textbox`;
 
-        // Add textarea and remove button to outter div
+        // Add textarea and remove button to outer div
         noteContainer.appendChild(textArea);
         noteContainer.appendChild(removeButton);
 
-        // Removes specified note and instantly updates localstorage JSON
+        // Add a click event listener to the remove button to delete the note
         removeButton.addEventListener("click", () => {
             this.remove();
         });
@@ -50,6 +60,7 @@ class Note {
         return noteContainer;
     }
 
+    // Method to create a note element for reading (non-editable)
     createReaderNoteElement() {
         const noteContainer = document.createElement("div");
         noteContainer.innerHTML = this.value;
@@ -60,6 +71,11 @@ class Note {
 
 }
 
+/**
+ * Creates a Note object and adds it to localstorage and appends writer element to DOM.
+ * 
+ * @param {string} key The identifier for the group of notes.
+ */
 function createNote(key) {
     const writerContainer = document.getElementById("writer-container");
     const note = new Note(key);
@@ -67,29 +83,52 @@ function createNote(key) {
     writerContainer.appendChild(note.createWriterNoteElement());
 }
 
+/**
+ * Returns the current Object of Notes from local storage, else an empty array.
+ * 
+ * @param {string} key The identifier for the group of Notes.
+ * @returns group of Notes
+ */
 function getDataFromLocalStorage(key) {
     return JSON.parse(localStorage.getItem(key)) || [];
 }
 
+/**
+ * Updates the localstorage of group of Notes with values on screen.
+ * 
+ * @param {string} key The identifier for the group of Notes.
+ */
 function intervalUpdateNotes(key) {
     const notesArr = getDataFromLocalStorage(key);
+    // Iterate through each onscreen note and if values differ from localstorage, update.
     notesArr.forEach((note) => {
         const noteContainer = document.getElementById(`${note.key}-${note.id}`);
         const textAreaElem = noteContainer.querySelector("textarea");
         const currNoteValue = textAreaElem.value;
-        if (note.value != currNoteValue) {
+        if (note.value !== currNoteValue) {
             note.value = currNoteValue;
         }
     });
     localStorage.setItem(key, JSON.stringify(notesArr));
 }
 
+/**
+ * Pushed Note object to localstorage.
+ * 
+ * @param {string} key The identifier for group of Notes.
+ * @param {Note} note Note object.
+ */
 function pushNote(key, note) {
     const notesArr = getDataFromLocalStorage(key);
     notesArr.push(note);
     localStorage.setItem(key, JSON.stringify(notesArr));
 }
 
+/**
+ * Renders reader view.
+ * 
+ * @param {string} key The identifier for group of Notes.
+ */
 function renderReader(key) {
     // Empty current view of reader container
     const readerContainer = document.getElementById("reader-container");
@@ -100,16 +139,23 @@ function renderReader(key) {
 
     // Generate and append reader note element
     notesArr.forEach(note => {
+        // Get note properties and recreate the Note object in order to use createWriterNoteElement().
         const noteObj = new Note(key, note.id, note.value);
         readerContainer.appendChild(noteObj.createReaderNoteElement());
     });
 }
 
+/**
+ * Renders writer view.
+ * 
+ * @param {string} key The identifier for group of Notes.
+ */
 function renderWriter(key) {
     const writerContainer = document.getElementById("writer-container");
     writerContainer.innerHTML = "";
     const notesArr = getDataFromLocalStorage(key);
     notesArr.forEach((note) => {
+        // Get note properties and recreate the Note object in order to use createWriterNoteElement().
         const noteObj = new Note(key, note.id, note.value);
         writerContainer.appendChild(noteObj.createWriterNoteElement());
     });
